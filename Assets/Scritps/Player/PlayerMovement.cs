6156c2cdc2c331aa8347879be983b0a10262602e
AronMarquezCh <99919382+AronMarquez@users.noreply.github.com>
2023-06-28 11:51:40 -0500

    Cuarta Parte
    Lesto lederes!!

--- Assets/Scritps/Player/PlayerMovement.cs
+++ Assets/Scritps/Player/PlayerMovement.cs
@@ -8,11 +8,27 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 12f;
 
+    private float gravity = -9.81f;
 
+    public Transform groundCheck;
+    public float sphereRadius = 0.3f;
+    public LayerMask groundMask;
 
+    bool isGrounded;
+
+    Vector3 velocity;
+
+    public float jumpHeight = 2;
 
     void Update()
     {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, sphereRadius,groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
 
         float X = Input.GetAxis("Horizontal");
 
@@ -20,7 +36,17 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * X + transform.forward * Z;
 
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
         characterController.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime);    
+
 
     }
 }
